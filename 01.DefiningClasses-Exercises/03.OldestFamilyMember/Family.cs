c33b97039c37fc8f34e20f8e0f5bb8ce93480c34
whywherewhat <mdsektor@gmail.com>
2018-02-15 18:55:09 +0000

    Added 03. Oldest Family Member

---
+++ 01.DefiningClasses-Exercises/03.OldestFamilyMember/Family.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Family
+{
+    private List<Person> members;
+
+    public List<Person> Members
+    {
+        get { return this.members; }
+    }
+
+    public Family()
+    {
+        this.members = new List<Person>();
+    }
+
+    public void AddMember(Person person)
+    {
+        this.members.Add(person);
+    }
+
+    public Person GetOldestMember()
+    {
+        return Members.OrderByDescending(a => a.Age).First();
+    }
+}
