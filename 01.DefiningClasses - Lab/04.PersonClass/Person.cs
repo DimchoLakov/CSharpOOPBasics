21b7bfb72dde622197e2a7abc2cf349eeec5158c
whywherewhat <mdsektor@gmail.com>
2018-02-15 18:24:35 +0000

    Added Solutions and Folder with Exercises

---
+++ 01.DefiningClasses - Lab/04.PersonClass/Person.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Person
+{
+    private string name;
+
+    public string Name
+    {
+        get { return this.name; }
+        set { this.name = value; }
+    }
+
+    private int age;
+
+    public int Age
+    {
+        get { return this.age; }
+        set { this.age = value; }
+    }
+
+    private List<BankAccount> Accounts { get; set; }
+
+    public Person()
+    {
+    }
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+        Accounts = new List<BankAccount>();
+    }
+
+    public Person(string name, int age, List<BankAccount> accounts) : this(name, age)
+    {
+        this.Accounts = accounts;
+    }
+
+
+    public decimal GetBalance()
+    {
+        return Accounts.Sum(x => x.Balance);
+    }
+}
---
+++ 01.DefiningClasses-Exercises/01.DefineAClassPerson/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+    static void Main()
+    {
+        Type personType = typeof(Person);
+        FieldInfo[] fields = personType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+        int fieldCount = fields.Length;
+        Console.WriteLine(fields.Length);
+
+        Person firstPerson = new Person
+        {
+            Name = "Pesho",
+            Age = 20
+        };
+
+        Person secondPerson = new Person
+        {
+            Name = "Gosho",
+            Age = 18
+        };
+
+        Person thirdPerson = new Person();
+        thirdPerson.Name = "Stamat";
+        thirdPerson.Age = 43;
+    }
+}
