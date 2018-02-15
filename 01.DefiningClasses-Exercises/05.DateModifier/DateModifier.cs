00d43569091b219d94cda8572db6847fecd592b9
whywherewhat <mdsektor@gmail.com>
2018-02-15 19:23:04 +0000

    Added 05. Date Modifier

---
+++ 01.DefiningClasses-Exercises/05.DateModifier/DateModifier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+
+public class DateModifier
+{
+    public int CalculateDaysBetweenDates(string firstDate, string secondDate)
+    {
+        DateTime first = DateTime.ParseExact(firstDate, "yyyy MM dd", CultureInfo.InvariantCulture);
+        DateTime second = DateTime.ParseExact(secondDate, "yyyy MM dd", CultureInfo.InvariantCulture);
+        return Math.Abs((int)(first - second).TotalDays);
+    }
+}
---
+++ 01.DefiningClasses-Exercises/05.DateModifier/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string firstInput = Console.ReadLine();
+        string secondInput = Console.ReadLine();
+
+        DateModifier dm = new DateModifier();
+        int difference = dm.CalculateDaysBetweenDates(firstInput, secondInput);
+
+        Console.WriteLine(difference);
+    }
+}
