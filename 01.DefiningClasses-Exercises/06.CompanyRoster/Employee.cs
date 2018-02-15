d85118b3ca8f60aebdba435046548be2d2187c1d
whywherewhat <mdsektor@gmail.com>
2018-02-15 20:41:31 +0000

    Added 06. Company Roster

---
+++ 01.DefiningClasses-Exercises/06.CompanyRoster/Employee.cs
@@ -0,0 +1,80 @@
+﻿public class Employee
+{
+    private string name;
+
+    public string Name
+    {
+        get { return this.name; }
+        set { this.name = value; }
+    }
+
+    private decimal salary;
+
+    public decimal Salary
+    {
+        get { return this.salary; }
+        set { this.salary = value; }
+    }
+
+    private string position;
+
+    public string Position
+    {
+        get { return this.position; }
+        set { this.position = value; }
+    }
+
+    private string department;
+
+    public string Department
+    {
+        get { return this.department; }
+        set { this.department = value; }
+    }
+
+    private string email;
+
+    public string Email
+    {
+        get { return this.email; }
+        set { this.email = value; }
+    }
+
+    private int age;
+
+    public int Age
+    {
+        get { return this.age; }
+        set { this.age = value; }
+    }
+
+    public Employee()
+    {
+        this.email = "n/a";
+        this.age = -1;
+    }
+
+    public Employee(string name, decimal salary, string position, string department) : this()
+    {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+    }
+
+    public Employee(string name, decimal salary, string position, string department, string email) : this(name, salary, position, department)
+    {
+        this.email = email;
+    }
+
+    public Employee(string name, decimal salary, string position, string department, int age) : this(name, salary, position, department)
+    {
+        this.age = age;
+    }
+
+    public Employee(string name, decimal salary, string position, string department, string email, int age) : this(name, salary, position, department)
+    {
+        this.email = email;
+        this.age = age;
+    }
+}
---
+++ 01.DefiningClasses-Exercises/06.CompanyRoster/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<Employee> employees = new List<Employee>();
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] tokens = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string name = tokens[0];
+            decimal salary = decimal.Parse(tokens[1]);
+            string position = tokens[2];
+            string department = tokens[3];
+
+            Employee employee = new Employee{Name = name, Salary = salary, Position = position, Department = department};
+
+            if (tokens.Length > 4)
+            {
+                if (tokens[4].Contains("@"))
+                {
+                    employee.Email = tokens[4];
+                }
+                else
+                {
+                    employee.Age = int.Parse(tokens[4]);
+                }
+            }
+
+            if (tokens.Length > 5)
+            {
+                employee.Age = int.Parse(tokens[5]);
+            }
+            employees.Add(employee);
+        }
+
+        var sortedEmployees = employees
+            .GroupBy(d => d.Department)
+            .Select(e => new
+            {
+                Department = e.Key,
+                AverageSalary = e.Average(ems => ems.Salary),
+                Employees = e.OrderByDescending(ems => ems.Salary)
+            }).OrderByDescending(s => s.AverageSalary)
+            .FirstOrDefault();
+
+        Console.WriteLine($"Highest Average Salary: {sortedEmployees.Department}");
+
+        foreach (Employee employee in sortedEmployees.Employees)
+        {
+            Console.WriteLine($"{employee.Name} {employee.Salary:f2} {employee.Email} {employee.Age}");
+        }
+    }
+}
