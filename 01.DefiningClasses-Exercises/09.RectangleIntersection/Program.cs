83169af71b0b5f5db8e3440d44e0dd63e1be60a0
whywherewhat <mdsektor@gmail.com>
2018-02-16 22:59:14 +0000

    Added 09. Rectangle Intersection

---
+++ 01.DefiningClasses-Exercises/09.RectangleIntersection/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] rectanglesAndChecks = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        int nRectangles = rectanglesAndChecks[0];
+        int checks = rectanglesAndChecks[1];
+
+        Dictionary<string, Rectangle> rectanglesDict = new Dictionary<string, Rectangle>();
+
+        for (int i = 0; i < nRectangles; i++)
+        {
+            string[] tokens = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string id = tokens[0];
+            double width = double.Parse(tokens[1]);
+            double height = double.Parse(tokens[2]);
+            double leftTopX = double.Parse(tokens[3]);
+            double leftTopY = double.Parse(tokens[4]);
+
+            Rectangle rectangle = new Rectangle(id, width, height, leftTopX, leftTopY);
+
+            if (!rectanglesDict.ContainsKey(id))
+            {
+                rectanglesDict.Add(id, rectangle);
+            }
+            else
+            {
+                rectanglesDict[id] = rectangle;
+            }
+        }
+
+        for (int i = 0; i < checks; i++)
+        {
+            string[] rectangleIDs = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (rectangleIDs.Length >= 2)
+            {
+                string first = rectangleIDs[0];
+                string second = rectangleIDs[1];
+                if (rectanglesDict.ContainsKey(first) && rectanglesDict.ContainsKey(second))
+                {
+                    Rectangle firstRectangle = rectanglesDict[first];
+                    Rectangle secondRectangle = rectanglesDict[second];
+                    Console.WriteLine(firstRectangle.Intersects(secondRectangle) ? "true" : "false");
+                }
+            }
+        }
+    }
+}
---
+++ 01.DefiningClasses-Exercises/09.RectangleIntersection/Rectangle.cs
@@ -0,0 +1,85 @@
+﻿using System.Threading;
+
+public class Rectangle
+{
+    private string id;
+
+    public string ID
+    {
+        get { return this.id; }
+        set { this.id = value; }
+    }
+
+    private double width;
+
+    public double Width
+    {
+        get { return this.width; }
+        set { this.width = value; }
+    }
+
+    private double height;
+
+    public double Height
+    {
+        get { return this.width; }
+        set { this.width = value; }
+    }
+
+    private double topLeftX;
+
+    public double TopLeftX
+    {
+        get { return this.topLeftX; }
+        set { this.topLeftX = value; }
+    }
+
+    private double topLeftY;
+
+    public double TopLeftY
+    {
+        get { return this.topLeftY; }
+        set { this.topLeftY = value; }
+    }
+
+    public Rectangle()
+    {
+    }
+
+    public Rectangle(string id, double width, double height, double topLeftX, double topLeftY)
+    {
+        this.id = id;
+        this.width = width;
+        this.height = height;
+        this.topLeftX = topLeftX;
+        this.topLeftY = topLeftY;
+    }
+
+    public bool Intersects(Rectangle rectangle)
+    {
+        return this.IsInIntersection(rectangle) || rectangle.IsInIntersection(this);
+    }
+
+    private bool IsInIntersection(Rectangle rectangle)
+    {
+        return (this.topLeftX >= rectangle.topLeftX &&
+                this.topLeftX <= rectangle.topLeftX + width &&
+                this.topLeftY + height >= rectangle.topLeftY &&
+                this.topLeftY + height <= rectangle.topLeftY + height)
+               ||
+               (this.topLeftX + width >= rectangle.topLeftX &&
+                this.topLeftX + width <= rectangle.topLeftX + width &&
+                this.topLeftY + width >= rectangle.topLeftY &&
+                this.topLeftY + width <= rectangle.topLeftY + height)
+               ||
+               (this.topLeftX >= rectangle.topLeftX &&
+                this.topLeftX <= rectangle.topLeftX + width &&
+                this.topLeftY >= rectangle.topLeftY &&
+                this.topLeftY <= rectangle.topLeftY + height)
+               ||
+               (this.topLeftX + width >= rectangle.topLeftX &&
+                this.topLeftX + width <= rectangle.topLeftX + width &&
+                this.topLeftY + height >= rectangle.topLeftY &&
+                this.topLeftY + height <= rectangle.topLeftY + height);
+    }
+}
