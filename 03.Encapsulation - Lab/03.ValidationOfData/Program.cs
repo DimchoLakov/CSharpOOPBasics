ef3b74f7c5664ee099fae2b11bf30190cada2a33
whywherewhat <mdsektor@gmail.com>
2018-02-20 20:10:25 +0000

    Added 03. Validation of Data

---
+++ 03.Encapsulation - Lab/03.ValidationOfData/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int lines = int.Parse(Console.ReadLine());
+        List<Person> people = new List<Person>();
+
+        for (int i = 0; i < lines; i++)
+        {
+            string[] tokens = Console.ReadLine().Split(new char[] { ' ' });
+
+            try
+            {
+                Person person = new Person(tokens[0], tokens[1], int.Parse(tokens[2]), decimal.Parse(tokens[3]));
+                people.Add(person);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        decimal percentage = decimal.Parse(Console.ReadLine());
+
+        people.ForEach(p => p.IncreaseSalary(percentage));
+        people.ForEach(Console.WriteLine);
+    }
+}
