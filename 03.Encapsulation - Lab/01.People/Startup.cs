0961e516751ff8ab11363e70a6961d9fd30b6dbc
whywherewhat <mdsektor@gmail.com>
2018-02-20 19:34:47 +0000

    Refactored 01. People

---
+++ 03.Encapsulation - Lab/01.People/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Startup
+{
+    public static void Main()
+    {
+        int lines = int.Parse(Console.ReadLine());
+        List<Person> people = new List<Person>();
+
+        for (int i = 0; i < lines; i++)
+        {
+            string[] tokens = Console.ReadLine().Split();
+            Person person = new Person(tokens[0], tokens[1], int.Parse(tokens[2]));
+
+            people.Add(person);
+        }
+
+        people.OrderBy(p => p.FirstName)
+            .ThenBy(p => p.Age)
+            .ToList()
+            .ForEach(Console.WriteLine);
+    }
+}
