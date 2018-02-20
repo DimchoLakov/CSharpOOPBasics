468a0a70c4e011d303e3aab6d15e9ea709d9e344
whywherewhat <mdsektor@gmail.com>
2018-02-20 21:24:17 +0000

    Added 04. Team

---
+++ 03.Encapsulation - Lab/04.Team/Person.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+public class Person
+{
+    private string firstName;
+    private string lastName;
+    private int age;
+    private decimal salary;
+
+    public string FirstName
+    {
+        get { return this.firstName; }
+        private set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("First name cannot contain fewer that 3 symbols!");
+            }
+            this.firstName = value;
+        }
+    }
+
+    public string LastName
+    {
+        get { return this.lastName; }
+        private set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Last name cannot contain fewer that 3 symbols!");
+            }
+            this.lastName = value;
+        }
+    }
+
+    public int Age
+    {
+        get { return this.age; }
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Age cannot be zero or a negative integer!");
+            }
+            this.age = value;
+        }
+    }
+
+    public decimal Salary
+    {
+        get { return this.salary; }
+        private set
+        {
+            if (value < 460)
+            {
+                throw new ArgumentException("Salary cannot be less than 460 leva!");
+            }
+            this.salary = value;
+        }
+    }
+
+    public Person(string firstName, string lastName, int age)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+    }
+
+    public Person(string firstName, string lastName, int age, decimal salary) : this (firstName, lastName, age)
+    {
+        this.Salary = salary;
+    }
+
+    public void IncreaseSalary(decimal percentage)
+    {
+        decimal bonus = this.salary * (percentage / (decimal)100m);
+
+        if (this.Age > 30)
+        {
+            this.salary += bonus;
+        }
+        else
+        {
+            this.salary += bonus / 2;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.FirstName} {this.LastName} receives {this.Salary:F2} leva.";
+    }
+}
---
+++ 03.Encapsulation - Lab/04.Team/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Startup
+{
+    static void Main()
+    {
+        int lines = int.Parse(Console.ReadLine());
+        List<Person> people = new List<Person>();
+
+        Team myTeam = new Team("SoftUni");
+
+        for (int i = 0; i < lines; i++)
+        {
+            string[] tokens = Console.ReadLine().Split();
+
+            Person person = new Person(tokens[0], tokens[1], int.Parse(tokens[2]), decimal.Parse(tokens[3]));
+            myTeam.AddPlayer(person);
+        }
+
+        Console.WriteLine(myTeam);
+    }
+}
