34e16f5bdd5486f9545e23292dc570268a2a5a7f
whywherewhat <mdsektor@gmail.com>
2018-02-20 22:40:37 +0000

    02. Class Box Data Validation

---
+++ 03.Encapsulation - Exercises/01.ClassBox/Box.cs
@@ -0,0 +1,45 @@
+﻿
+public class Box
+{
+    private double length;
+    private double width;
+    private double height;
+
+    public double Length
+    {
+        get { return this.length; }
+        private set { this.length = value; }
+    }
+
+    public double Width
+    {
+        get { return this.width; }
+        private set { this.width = value; }
+    }
+
+    public double Height
+    {
+        get { return this.height; }
+        private set { this.height = value; }
+    }
+
+    public Box(double length, double width, double height)
+    {
+        this.Length = length;
+        this.Width = width;
+        this.Height = height;
+    }
+
+    public double CalculateSurfaceArea()
+    {
+        return 2 * this.Length * this.Width + 2 * this.Length * this.Height + 2 * this.Width * this.Height;
+    }
+    public double CalculateLateralSurface()
+    {
+        return 2 * this.Length * this.Height + 2 * this.Width * this.Height;
+    }
+    public double CalculateVolume()
+    {
+        return this.Length * this.Width * this.Height;
+    }
+}
---
+++ 03.Encapsulation - Exercises/02.ClassBoxDataValidation/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Startup
+{
+    static void Main()
+    {
+        double length = double.Parse(Console.ReadLine());
+        double width = double.Parse(Console.ReadLine());
+        double height = double.Parse(Console.ReadLine());
+
+        Box box;
+        try
+        {
+            box = new Box(length, width, height);
+            Console.WriteLine($"Surface Area - {box.CalculateSurfaceArea():f2}");
+            Console.WriteLine($"Lateral Surface Area - {box.CalculateLateralSurface():f2}");
+            Console.WriteLine($"Volume - {box.CalculateVolume():f2}");
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception.Message);
+        }
+    }
+}
