0008371ed5373b359fdf572067e519d90cef0104
whywherewhat <mdsektor@gmail.com>
2018-02-21 22:25:45 +0000

    Added 05. Pizza Calories

---
+++ 03.Encapsulation - Exercises/05.PizzaCalories/Dough.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+public class Dough
+{
+    private string type;
+    private string bakingTechnique;
+    private double weight;
+
+    private string Type
+    {
+        get { return this.type; }
+        set
+        {
+            if (value.ToLower() != "white" && value.ToLower() != "wholegrain")
+            {
+                throw new ArgumentException("Invalid type of dough.");
+            }
+            this.type = value;
+        }
+    }
+
+    private string BakingTechnique
+    {
+        get { return this.bakingTechnique; }
+        set
+        {
+            if (value.ToLower() != "crispy" && value.ToLower() != "chewy" && value.ToLower() != "homemade")
+            {
+                throw new ArgumentException("Invalid type of dough.");
+            }
+            this.bakingTechnique = value;
+        }
+    }
+
+    private double Weight
+    {
+        get { return this.weight; }
+        set
+        {
+            if (value < 1 || value > 200)
+            {
+                throw new ArgumentException("Dough weight should be in the range [1..200].");
+            }
+            this.weight = value;
+        }
+    }
+
+    public Dough()
+    {
+    }
+
+    public Dough(string type, string bakingTechnique, double weight)
+    {
+        this.Type = type;
+        this.BakingTechnique = bakingTechnique;
+        this.Weight = weight;
+    }
+
+    public double GetCalories()
+    {
+        double baseModifier = 2d;
+        double typeModifier = 0d;
+
+        switch (this.Type.ToLower())
+        {
+            case "white":
+                typeModifier = 1.5d;
+                break;
+            case "wholegrain":
+                typeModifier = 1d;
+                break;
+        }
+
+        double bakingTechniqueModifier = 0d;
+
+        switch (this.BakingTechnique.ToLower())
+        {
+            case "crispy":
+                bakingTechniqueModifier = 0.9d;
+                break;
+            case "chewy":
+                bakingTechniqueModifier = 1.1d;
+                break;
+            case "homemade":
+                bakingTechniqueModifier = 1d;
+                break;
+        }
+
+        return this.Weight * baseModifier * typeModifier * bakingTechniqueModifier;
+    }
+}
---
+++ 03.Encapsulation - Exercises/05.PizzaCalories/Topping.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+public class Topping
+{
+    private string type;
+    private double weight;
+
+    private string Type
+    {
+        get { return this.type; }
+        set
+        {
+            if (value.ToLower() != "meat" && value.ToLower() != "veggies" && value.ToLower() != "cheese" && value.ToLower() != "sauce")
+            {
+                throw new ArgumentException($"Cannot place {value} on top of your pizza.");
+            }
+            this.type = value;
+        }
+    }
+
+    private double Weight
+    {
+        get { return this.weight; }
+        set
+        {
+            if (value < 1 || value > 50)
+            {
+                throw new ArgumentException($"{this.Type} weight should be in the range [1..50].");
+            }
+            this.weight = value;
+        }
+    }
+
+    public Topping()
+    {
+    }
+
+    public Topping(string type, double weight)
+    {
+        this.Type = type;
+        this.Weight = weight;
+    }
+
+    public double GetCalories()
+    {
+        double baseModifier = 2d;
+        double typeModifier = 0d;
+
+        switch (this.Type.ToLower())
+        {
+            case "meat":
+                typeModifier = 1.2d;
+                break;
+            case "veggies":
+                typeModifier = 0.8d;
+                break;
+            case "cheese":
+                typeModifier = 1.1d;
+                break;
+            case "sauce":
+                typeModifier = 0.9d;
+                break;
+        }
+
+        return this.Weight * baseModifier * typeModifier;
+    }
+}
