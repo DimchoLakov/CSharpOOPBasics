703414d0f6c56d60cfe4ad12a20c792dcb89db80
whywherewhat <mdsektor@gmail.com>
2018-02-24 00:34:58 +0000

    Added 04. Inheritance - Exercies 01. Person

---
+++ 04.Inheritance - Exercises/01.Person/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Person
+{
+    private string name;
+
+    public string Name
+    {
+        get { return this.name; }
+        set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Name's length should not be less than 3 symbols!");
+            }
+            this.name = value;
+        }
+    }
+
+    private int age;
+
+    public virtual int Age
+    {
+        get { return this.age; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Age must be positive!");
+            }
+            this.age = value;
+        }
+    }
+
+    public Person()
+    {
+    }
+
+    public Person(string name, int age) : this()
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {this.Name}, Age: {this.Age}";
+    }
+}
