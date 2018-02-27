ec5cb8de7c4fc5b2486f4a4d10b5592e13248309
whywherewhat <mdsektor@gmail.com>
2018-02-27 22:25:00 +0000

    Added 05. Interfaces and Abstraction - Lab  01. Shapes

---
+++ 05.Interfaces And Abstraction - Lab/01.Shapes/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        int radius = int.Parse(Console.ReadLine());
+        Circle circle = new Circle(radius);
+        circle.Draw();
+
+        int width = int.Parse(Console.ReadLine());
+        int height = int.Parse(Console.ReadLine());
+        Rectangle rectangle = new Rectangle(width, height);
+        rectangle.Draw();
+    }
+}
