fe6935efd083a446473221fe49f068c27a6a3acd
whywherewhat <mdsektor@gmail.com>
2018-02-27 23:02:54 +0000

    Added 05.Interfaces And Abstraction - Exercises 01.DefineAnInterfaceIPerson

---
+++ 05.Interfaces And Abstraction - Exercises/01.DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Citizen : IPerson
+{
+    public Citizen(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public override string ToString()
+    {
+        return $"{this.Name}{Environment.NewLine}{this.Age}";
+    }
+}
---
+++ 05.Interfaces And Abstraction - Exercises/01.DefineAnInterfaceIPerson/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        IPerson citizen = new Citizen(name, age);
+        Console.WriteLine(citizen.ToString());
+    }
+}
