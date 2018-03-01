3b55a7c18087578016e0e6c5d8eda26e6c19e968
whywherewhat <mdsektor@gmail.com>
2018-03-01 23:37:47 +0000

    Added 05.Border Control

---
+++ 05.Interfaces And Abstraction - Exercises/05.BorderControl/Citizen.cs
@@ -0,0 +1,18 @@
+﻿public class Citizen : IIdentifyable
+{
+    public string Name { get; private set; }
+
+    public int Age { get; private set; }
+
+    public string Id { get; private set; }
+    public Citizen()
+    {
+    }
+
+    public Citizen(string name, int age, string id) : this()
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Id = id;
+    }
+}
---
+++ 05.Interfaces And Abstraction - Exercises/05.BorderControl/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        List<IIdentifyable> rebelions = new List<IIdentifyable>();
+
+        string input = Console.ReadLine();
+
+        while (input != "End")
+        {
+            string[] tokens = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (tokens.Length == 2)
+            {
+                string model = tokens[0];
+                string id = tokens[1];
+
+                Robot robot = new Robot(model, id);
+                rebelions.Add(robot);
+            }
+            else if (tokens.Length == 3)
+            {
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+                string id = tokens[2];
+
+                Citizen citizen = new Citizen(name, age, id);
+                rebelions.Add(citizen);
+            }
+
+            input = Console.ReadLine();
+        }
+
+        string lastDigitsOfFakeId = Console.ReadLine();
+
+        List<string> citizensFakeIds = rebelions
+            .Where(c => c.Id.EndsWith(lastDigitsOfFakeId))
+            .Select(c => c.Id)
+            .ToList();
+
+        citizensFakeIds.ForEach(Console.WriteLine);
+    }
+}
