2d853494b30b22271b1aba8891beb4e6042e9e31
whywherewhat <mdsektor@gmail.com>
2018-03-03 23:08:45 +0000

    Added 10. Explicit Interfaces

---
+++ 05.Interfaces And Abstraction - Exercises/10.ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        List<Citizen> citizens = new List<Citizen>();
+
+        string input = Console.ReadLine();
+
+        while (input != "End")
+        {
+            string[] tokens = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            citizens.Add(new Citizen(tokens[0], tokens[1], int.Parse(tokens[2])));
+
+            input = Console.ReadLine();
+        }
+
+        foreach (Citizen citizen in citizens)
+        {
+            //IPerson iPerson = (IPerson)citizen;
+            //IResident iResident = (IResident)citizen;
+            //Console.WriteLine(iPerson.GetName());
+            //Console.WriteLine(iResident.GetName());
+
+            Console.WriteLine(((IPerson)citizen).GetName());
+            Console.WriteLine(((IResident)citizen).GetName());
+        }
+    }
+}
