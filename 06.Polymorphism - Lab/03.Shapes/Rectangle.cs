320450987b5e67d64cacc659cae6dbf1866a2e74
whywherewhat <mdsektor@gmail.com>
2018-03-04 00:30:21 +0000

    Added 03. Shapes

---
+++ 06.Polymorphism - Lab/03.Shapes/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿public class Rectangle : Shape
+{
+    public double Height { get; private set; }
+    public double Width { get; private set; }
+
+    public Rectangle(double height, double width)
+    {
+        this.Height = height;
+        this.Width = width;
+    }
+
+    public override double CalculatePerimeter()
+    {
+        return 2 * (this.Height + this.Width);
+    }
+
+    public override double CalculateArea()
+    {
+        return this.Height * this.Width;
+    }
+
+    public override string Draw()
+    {
+        return base.Draw() + "Rectangle";
+    }
+}
---
+++ 06.Polymorphism - Lab/03.Shapes/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class StartUp
+{
+    static void Main()
+    {
+        List<Shape> shapes = new List<Shape>
+        {
+            new Rectangle(3.5, 4.3),
+            new Rectangle(2.5, 7.6),
+            new Rectangle(7.1, 6.8),
+            new Circle(5.5),
+            new Circle(7.3),
+            new Circle(8.4)
+        };
+
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.Draw());
+        }
+    }
+}
