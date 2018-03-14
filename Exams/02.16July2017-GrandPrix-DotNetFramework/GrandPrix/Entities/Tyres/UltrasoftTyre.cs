fd8de5ad2a857921c8a2d54099c2e76387baef6b
whywherewhat <mdsektor@gmail.com>
2018-03-14 14:25:50 +0000

    Added Exams Folder

---
+++ Exams/02.16July2017-GrandPrix-DotNetFramework/GrandPrix/Entities/Tyres/UltrasoftTyre.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class UltrasoftTyre : Tyre
+{
+    public UltrasoftTyre(double hardness, double grip) : base(hardness)
+    {
+        this.Grip = grip;
+    }
+
+    public override string Name => "Ultrasoft";
+
+    private double grip;
+
+    public double Grip
+    {
+        get { return this.grip; }
+        private set { this.grip = value; }
+    }
+
+    private double degradation;
+
+    public override double Degradation
+    {
+        get { return this.degradation; }
+        protected set
+        {
+            if (value < 30)
+            {
+                throw new ArgumentException("Blown Tyre");
+            }
+            this.degradation = value;
+        }
+    }
+
+    public override void Degradate()
+    {
+        this.Degradation -= this.Hardness + this.Grip;
+    }
+}
---
+++ Exams/02.16July2017-GrandPrix-DotNetFramework/GrandPrix/Factories/DriverFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class DriverFactory
+{
+    public static Driver CreateDriver(List<string> arguments)
+    {
+        string type = arguments[0];
+        string name = arguments[1];
+
+        Tyre tyre = TyreFactory.CreateTyre(arguments);
+
+        Car car = new Car(int.Parse(arguments[2]), double.Parse(arguments[3]), tyre);
+
+        switch (type)
+        {
+            case "Aggressive":
+                return new AggressiveDriver(name, car);
+            case "Endurance":
+                return new EnduranceDriver(name, car);
+            default:
+                throw new ArgumentException();
+        }
+    }
+}
