c5c7a62b88b7ca11a1b35d037010d6b23e27f8d5
whywherewhat <mdsektor@gmail.com>
2018-03-14 14:44:57 +0000

    Folder Name Changed

--- Exams/02.16July2017-GrandPrix-DotNetFramework/GrandPrix/Entities/Drivers/EnduranceDriver.cs
+++
@@ -1,7 +0,0 @@
-﻿public class EnduranceDriver : Driver
-{
-    public EnduranceDriver(string name, Car car) : base(name, car)
-    {
-        this.FuelConsumptionPerKm = 1.5;
-    }
-}
--- Exams/02.16July2017-GrandPrix-DotNetFramework/GrandPrix/StartUp.cs
+++
@@ -1,9 +0,0 @@
-﻿public class StartUp
-{
-    public static void Main()
-    {
-        RaceTower raceTower = new RaceTower();
-        Engine engine = new Engine(raceTower);
-        engine.Start();
-    }
-}
